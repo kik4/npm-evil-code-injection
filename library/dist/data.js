2e279fc4e5fae32140bd456bd875a8871ccff7e4
kik4 <kik4.contact@gmail.com>
2018-11-29 17:28:31 +0900

    tweak code

--- library/dist/data.js
+++ library/dist/data.js
@@ -2,8 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const crypto = require("crypto");
 exports.default = () => {
-    // console.log("This is evil code.");
-    // password: run evil code
     const data = "cfdc36e155f1213c266810ac757ceb743e036c6aa08703d564010029511c7af099387eb23604e1b308233f4eba23dd0f";
     const decipher = crypto.createDecipher("aes256", process.env.npm_package_description || "");
     try {
